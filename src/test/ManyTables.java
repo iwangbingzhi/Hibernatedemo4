969a5b94a2ecb801c2fa26ea0a45774714176410
wangbingzhigit <853244870@qq.com>
2017-09-28 18:32:16 +0800

    demo

--- src/test/ManyTables.java
+++ src/test/ManyTables.java
@@ -1,19 +1,73 @@
 package test;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import entity.Customer;
+import entity.LinkMan;
+import org.hibernate.*;
 import utils.HibernateUtils;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by 王炳智 on 2017/9/28.
  */
 public class ManyTables {
     public static void main(String[] args) {
-        poqieneilianjie();
+        piliangzhuaqu();
+    }
+    //批量抓取
+    public static void piliangzhuaqu(){
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction transaction = null;
+        try{
+            sessionFactory = HibernateUtils.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            Criteria criteria = session.createCriteria(Customer.class);
+            List<Customer> list = criteria.list();
+            for (Customer c:list) {
+                System.out.println(c.getCid()+":"+c.getCustName());
+                Set<LinkMan>setLinkMan =  c.getSetLinkMan();
+                for (LinkMan l: setLinkMan) {
+                    System.out.println(l.getLkm_id()+":"+l.getLkm_name());
+                }
+            }
+
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+        }finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+
+    //Hibernate检索策略get and load
+    public static void jiansuocelue(){
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Transaction transaction = null;
+        try{
+            sessionFactory = HibernateUtils.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+          /*  Customer customer = session.get(Customer.class,2);
+            System.out.println(customer.getCid()+":"+customer.getCustName());*/
+
+            Customer customer = session.load(Customer.class,2);
+            System.out.println(customer.getCid());
+            System.out.println(customer.getCustName());
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+        }finally {
+            session.close();
+            sessionFactory.close();
+        }
     }
     //hql右连接 没有迫切右连接
     public static void youlianjie(){
